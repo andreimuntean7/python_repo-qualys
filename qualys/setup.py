d720a960846329007879ab0d80f08d343db37dd8
Andrei Muntean <andreimuntean0795@gmail.com>
2022-05-24 13:18:21 +0000

    test

---
+++ qualys/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='qualys',
+    packages=['qualys'],
+    description='Hello world enterprise edition',
+    version='0.2',
+    url='https://github.com/andreimuntean7/test_python_repo/qualys',
+    author='Andrei Muntean',
+    author_email='andreimuntean0795@gmail.com',
+    keywords=['pip','qualys','example'],
+    install_requires=["python_version>'3.6'",
+    "requests>'2.0'",
+    "elasticsearch>='8.2'"]
+)
